c74017579ac356a1ed49ee0e7bc1526a15d4915e
Federico Bertolini <fb@hellofresh.com>
2017-11-19 13:43:08 +0100

    remove text nodes

--- parser/index.js
+++ parser/index.js
@@ -14,49 +14,38 @@ exports.parseHtmlString = (htmlString) => {
     const body = findNodeChild(htmlElement, 'body');
 
     return {
-        head: parseNode(head),
-        body: parseNode(body),
+        head: parseTag(head),
+        body: parseTag(body),
     };
 };
 
 const findNodeChild = (element, nodeName) =>
     element.childNodes.find(node => node.nodeName === nodeName);
 
-const parseNode = (nodeData) => {
-    switch(nodeData.nodeName) {
-        case '#comment':
-            return null;
-        case '#text':
-            return nodeData.value.trim() ? parseText(nodeData) : null;
-        default:
-            return parseTag(nodeData);
-    }
-};
-
 const parseTag = (tagNode) => {
     const tag = {
         nodeType: 'tag',
         tagName: tagNode.tagName,
         attributes: {},
-        estimatedSize: 0
+        estimatedSize: 0,
+        children: [],
     };
     tagNode.attrs.forEach((attr) => {
         tag.attributes[attr.name] = attr.value;
         tag.estimatedSize += attr.name.length + (attr.value || '').length + 4;
     });
-    tag.children = tagNode.childNodes.map(parseNode).filter(n => n !== null);
+
+    tagNode.childNodes.forEach(childNode => {
+        if (childNode.nodeName === '#text') {
+            const size = childNode.value.trim().length;
+            tag.estimatedSize += size;
+        } else if (childNode.nodeName !== '#comment') {
+            tag.children.push(parseTag(childNode));
+        }
+    });
 
     tag.estimatedSize += tag.children.reduce((size, node) => size + node.estimatedSize, 0);
     tag.estimatedSize += (tag.tagName.length * 2) + 5;
 
     return tag;
-};
-
-const parseText = (textNode) => {
-    const text = {
-        nodeType: 'text',
-        textValue: textNode.value.trim(),
-    };
-    text.estimatedSize = text.textValue.length;
-    return text;
 };
