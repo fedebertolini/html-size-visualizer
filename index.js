6f2bbd06fe1f08cc5176e1e8b39f9350e56bdf54
Federico Bertolini <fedebertolini@gmail.com>
2020-11-14 13:35:58 +0100

    use sade to manage CLI

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
+const sade = require('sade');
 const axios = require('axios');
-const minimist = require('minimist');
+const fs = require('fs');
 const parser = require('./parser');
 const visualizer = require('./visualizer');
 
-const argv = minimist(process.argv.slice(2));
-if (!argv.path && !argv.url) {
-    console.log('Missing required parameter. Please provide `path` or `url`');
-    return;
-}
-
-let domTree;
-if (argv.path) {
-    domTree = parser.parseFile(argv.path);
-    visualizer.render(domTree);
-} else {
-    const url = argv.url.startsWith('http') ? argv.url : ('http://' + argv.url);
-
-    axios.get(url).then(result => {
-        domTree = parser.parseHtmlString(result.data);
-        visualizer.render(domTree);
-    });
-}
+(async () => {
+    sade('html-size-visualizer [url|filePath]', true)
+        .describe('Visualize an HTML document as a tree and detect the biggest sub-trees')
+        .example('http://www.example.com')
+        .example('./my-homepage.html')
+        .action(async (arg) => {
+            if (typeof arg === 'undefined') {
+                console.error('Missing required argument, please provide a URL or a path to an HTML file');
+                process.exit(1);
+            }
+            let html = null;
+            if (arg.startsWith('http')) {
+                try {
+                    const response = await axios.get(arg);
+                    html = response.data;
+                } catch(err) {
+                    console.error(`Error trying to fetch html from ${arg}`, err);
+                    process.exit(1);
+                }
+            } else if (fs.existsSync(arg)) {
+                html = fs.readFileSync(arg, 'utf-8');
+            } else {
+                console.error(`There is no file in path ${arg}`);
+                process.exit(1);
+            }
+            const domTree = parser.parseHtmlString(html);
+            visualizer.render(domTree);
+        })
+        .parse(process.argv);
+})();
