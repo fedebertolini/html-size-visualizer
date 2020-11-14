333435964a6f8837ff6ae5624646aafe26947cca
Federico Bertolini <fedebertolini@gmail.com>
2020-11-14 14:09:57 +0100

    create report file in temp dir

--- visualizer/index.js
+++ visualizer/index.js
@@ -2,11 +2,14 @@ const Mustache = require('mustache');
 const fs = require('fs');
 const path = require('path');
 const open = require('open');
+const tmp = require('tmp');
 
 exports.render = (data) =>
     readVisualizerFiles()
     .then(files => {
-        const distPath = path.join(__dirname, '..', 'dist', `${Date.now()}.html`);
+        const tmpobj = tmp.fileSync({ mode: 0o644, prefix: 'html-size-visualizer', postfix: '.html' });
+        const distPath = tmpobj.name;
+
         const renderParams = {
             data: JSON.stringify(data),
             styles: files.styles,
@@ -14,6 +17,7 @@ exports.render = (data) =>
         };
         const rendered = Mustache.render(files.template, renderParams);
         fs.writeFileSync(distPath, rendered, 'utf8');
+        console.log('Report file created', distPath)
         open(distPath, { wait: false });
     });
 
