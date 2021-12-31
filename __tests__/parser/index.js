d45b762ff4f5173ea162d0bbf38df77f67042e67
Federico Bertolini <fedebertolini@gmail.com>
2021-12-31 12:52:13 -0300

    replace jest with uvu

--- __tests__/parser/index.js
+++ __tests__/parser/index.js
@@ -1,10 +1,69 @@
+const { test } = require("uvu");
+const assert = require("uvu/assert");
 const parser = require("../../parser");
 
-describe("parser", () => {
-  it("should parse the simple html file", () => {
-    const filePath = process.cwd() + "/__tests__/parser/files/simple.html";
+test("should parse the simple html file", () => {
+  const filePath = process.cwd() + "/__tests__/parser/files/simple.html";
 
-    const domTree = parser.parseFile(filePath);
-    expect(domTree).toMatchSnapshot();
+  const domTree = parser.parseFile(filePath);
+
+  assert.equal(domTree, {
+    nodeType: "tag",
+    tagName: "html",
+    attributes: {},
+    estimatedSize: 220,
+    children: [
+      {
+        nodeType: "tag",
+        tagName: "head",
+        attributes: {
+          lang: "en",
+        },
+        estimatedSize: 122,
+        children: [
+          {
+            nodeType: "tag",
+            tagName: "title",
+            attributes: {},
+            estimatedSize: 26,
+            children: [],
+          },
+          {
+            nodeType: "tag",
+            tagName: "meta",
+            attributes: {
+              name: "description",
+              content: "This is a simple HTML document",
+            },
+            estimatedSize: 73,
+            children: [],
+          },
+        ],
+      },
+      {
+        nodeType: "tag",
+        tagName: "body",
+        attributes: {},
+        estimatedSize: 85,
+        children: [
+          {
+            nodeType: "tag",
+            tagName: "h1",
+            attributes: {},
+            estimatedSize: 20,
+            children: [],
+          },
+          {
+            nodeType: "tag",
+            tagName: "p",
+            attributes: {},
+            estimatedSize: 37,
+            children: [],
+          },
+        ],
+      },
+    ],
   });
 });
+
+test.run();
