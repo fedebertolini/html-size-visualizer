49a60b57aded432afebb97d30c08c12f84437efb
Federico Bertolini <fedebertolini@gmail.com>
2021-12-31 13:07:46 -0300

    replace chalk with kleur

--- index.js
+++ index.js
@@ -1,42 +1,55 @@
 #!/usr/bin/env node
 
-const sade = require('sade');
-const axios = require('axios');
-const chalk = require('chalk');
-const fs = require('fs');
-const pck = require('./package.json');
-const parser = require('./parser');
-const visualizer = require('./visualizer');
+const sade = require("sade");
+const axios = require("axios");
+const kleur = require("kleur");
+const fs = require("fs");
+const pck = require("./package.json");
+const parser = require("./parser");
+const visualizer = require("./visualizer");
 
 (async () => {
-    sade('html-size-visualizer [url|filePath]', true)
-        .version(pck.version)
-        .describe('Visualize an HTML document as a tree and detect the biggest sub-trees')
-        .example('http://www.example.com')
-        .example('./my-homepage.html')
-        .action(async (arg) => {
-            if (typeof arg === 'undefined') {
-                console.error(chalk.red('Missing required argument, please provide a URL or a path to an HTML file'));
-                console.log(`To learn more run ${chalk.magenta("html-size-visualizer --help")}`);
-                process.exit(1);
-            }
-            let html = null;
-            if (arg.startsWith('http')) {
-                try {
-                    const response = await axios.get(arg);
-                    html = response.data;
-                } catch(err) {
-                    console.error(chalk.red(`Error trying to fetch html from ${chalk.magenta(arg)}`), err);
-                    process.exit(1);
-                }
-            } else if (fs.existsSync(arg)) {
-                html = fs.readFileSync(arg, 'utf-8');
-            } else {
-                console.error(chalk.red(`There is no file in path ${chalk.magenta(arg)}`));
-                process.exit(1);
-            }
-            const domTree = parser.parseHtmlString(html);
-            visualizer.render(domTree);
-        })
-        .parse(process.argv);
+  sade("html-size-visualizer [url|filePath]", true)
+    .version(pck.version)
+    .describe(
+      "Visualize an HTML document as a tree and detect the biggest sub-trees"
+    )
+    .example("http://www.example.com")
+    .example("./my-homepage.html")
+    .action(async (arg) => {
+      if (typeof arg === "undefined") {
+        console.error(
+          kleur.red(
+            "Missing required argument, please provide a URL or a path to an HTML file"
+          )
+        );
+        console.log(
+          `To learn more run ${kleur.magenta("html-size-visualizer --help")}`
+        );
+        process.exit(1);
+      }
+      let html = null;
+      if (arg.startsWith("http")) {
+        try {
+          const response = await axios.get(arg);
+          html = response.data;
+        } catch (err) {
+          console.error(
+            kleur.red(`Error trying to fetch html from ${kleur.magenta(arg)}`),
+            err.message
+          );
+          process.exit(1);
+        }
+      } else if (fs.existsSync(arg)) {
+        html = fs.readFileSync(arg, "utf-8");
+      } else {
+        console.error(
+          kleur.red(`There is no file in path ${kleur.magenta(arg)}`)
+        );
+        process.exit(1);
+      }
+      const domTree = parser.parseHtmlString(html);
+      visualizer.render(domTree);
+    })
+    .parse(process.argv);
 })();
--- visualizer/index.js
+++ visualizer/index.js
@@ -1,50 +1,53 @@
-const Mustache = require('mustache');
-const fs = require('fs');
-const chalk = require('chalk');
-const path = require('path');
-const open = require('open');
-const tmp = require('tmp');
+const Mustache = require("mustache");
+const fs = require("fs");
+const kleur = require("kleur");
+const path = require("path");
+const open = require("open");
+const tmp = require("tmp");
 
 exports.render = (data) =>
-    readVisualizerFiles()
-    .then(files => {
-        const tmpobj = tmp.fileSync({ mode: 0o644, prefix: 'html-size-visualizer', postfix: '.html' });
-        const distPath = tmpobj.name;
-
-        const renderParams = {
-            data: JSON.stringify(data),
-            styles: files.styles,
-            script: files.script,
-        };
-        const rendered = Mustache.render(files.template, renderParams);
-        fs.writeFileSync(distPath, rendered, 'utf8');
-        console.log(`Report file created in ${chalk.magenta(distPath)}`)
-        open(distPath, { wait: false });
+  readVisualizerFiles().then((files) => {
+    const tmpobj = tmp.fileSync({
+      mode: 0o644,
+      prefix: "html-size-visualizer",
+      postfix: ".html",
     });
+    const distPath = tmpobj.name;
+
+    const renderParams = {
+      data: JSON.stringify(data),
+      styles: files.styles,
+      script: files.script,
+    };
+    const rendered = Mustache.render(files.template, renderParams);
+    fs.writeFileSync(distPath, rendered, "utf8");
+    console.log(`Report file created in ${kleur.magenta(distPath)}`);
+    open(distPath, { wait: false });
+  });
 
 const readVisualizerFiles = () => {
-    const templatePath = path.join(__dirname, 'template.mustache');
-    const scriptPath = path.join(__dirname, 'script.js');
-    const stylesPath = path.join(__dirname, 'styles.css');
+  const templatePath = path.join(__dirname, "template.mustache");
+  const scriptPath = path.join(__dirname, "script.js");
+  const stylesPath = path.join(__dirname, "styles.css");
 
-    return Promise.all([
-        readFile(templatePath),
-        readFile(scriptPath),
-        readFile(stylesPath),
-    ])
-    .then(result => ({
-        template: result[0],
-        script: result[1],
-        styles: result[2],
-    }));
+  return Promise.all([
+    readFile(templatePath),
+    readFile(scriptPath),
+    readFile(stylesPath),
+  ]).then((result) => ({
+    template: result[0],
+    script: result[1],
+    styles: result[2],
+  }));
 };
 
-const readFile = path => new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-        if (err) {
-            reject(err);
-            return;
-        }
-        resolve(data);
+const readFile = (path) =>
+  new Promise((resolve, reject) => {
+    fs.readFile(path, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(data);
     });
-});
+  });
